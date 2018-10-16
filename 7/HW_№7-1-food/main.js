e85c55bd1ae0bbf9460a8b5ac65cfea27e5ab7e2
krivonosoven82 <krivonosoven82@gmail.com>
2018-10-16 22:07:23 +0300

    wh-7.1

--- 7/HW_№7-1-food/main.js
+++ 7/HW_№7-1-food/main.js
@@ -1,49 +1,36 @@
-const products = {
-    'water': 1000,
-    'pea': 2,
-    'corn': 1,
-    'tomato': 3,
-    'hot': 2,
-    'mushroom': 6,
-    'potato': 10,
-    'celery': 2,
-    'cucumber': 3,
-    'carrot': 6,
-    'onion': 7,
-    'vegetablesOil': 10,
-    'meat': 3,
-    'milk': 3,
-    'coffee': 3,
-    'blackTea': 3,
-    'greenTea': 3,
-    'salt': 5
+// продукты
+let products = {
+    tea: 100,
+    milk: 40,
+    coffee: 40,
+    sugar: 30,
+    water: 100,
+    bread: 600,
+    salt: 300,
+    pepper: 500
 };
-//----------------------
-const mushroomSoup = {
-    'water': 1.5,
-    'mushroom': 6,
-    'potato': .35,
-    'carrot': .15,
-    'onion': .15,
-    'vegetablesOil': .06,
-    'salt': .03
+
+// рецепт
+let recipe = {
+    milk: 10,
+    coffee: 10,
+    sugar: 5,
+    water: 20
 };
 
 
-let obj = {};
 
-let key;
-let count = 1;
-let arr = [];
-let i;
+function toCalcPortion(products, recipe) {
+    let proportions = [];
+    let key;
+    for (key in recipe) {
+        if (products[key] && products[key] >= recipe[key]) {
+            proportions.push(Math.floor(products[key] / recipe[key]));
+        } else {
+            return console.log('Не хватает ингридиентов');
+        }
+    }
+    return console.log(`Продуктов хватит на проций: ${Math.min(...proportions)}`);
+}
 
-// for(key in products){
-//     console.log(`product ${key} = ${products[key]} count = ${count++}`);
-//     arr.push(key);
-//
-// }
-// // for(i = 0; i < ){}
-// console.log(arr);
-
-console.log();
-
+toCalcPortion(products, recipe);
