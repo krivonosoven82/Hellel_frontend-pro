3ad3d105d80864da8dad5679af158f6822e5c94b
krivonosoven82 <krivonosoven82@gmail.com>
2018-10-20 10:47:13 +0300

    hw_9.1 modify.0.0.2

--- 9/HW_№9-1-apply/main.js
+++ 9/HW_№9-1-apply/main.js
@@ -9,10 +9,28 @@ function mul() {
         return number1 * number2;
     });
 }
+
 function applyAll(func){
     return func.apply(this, [].slice.call(arguments, 1));
 }
 
 console.log(applyAll(sum, 1, 2, 3));
 console.log(applyAll(mul, 1, 2, 3, 4));
+
+//----------ES6---------------------------------------------------------------------------------------------------------
+
+function sumES6(...res){
+    return res.reduce((accumulator, currentValue) => accumulator + currentValue);
+}
+
+function mulES6 (...res) {
+    return res.reduce((accumulator, currentValue) => accumulator * currentValue);
+}
+
+function applyAllES6(func, ...res){
+   return func.apply(this, res);
+}
+
+console.log(applyAllES6(sumES6, 1, 2, 3));
+console.log(applyAllES6(mulES6, 1, 2, 3, 4));
 
